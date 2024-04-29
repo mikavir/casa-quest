5a36300bf0f9537e481cd7b05cdfbbde1dad9260
Mika Virtucio <mikavirtucio@gmail.com>
2024-04-29 14:24:07 +0000

    Add registration functionality

--- app.py
+++ app.py
@@ -1,15 +1,55 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
-@app.route("/")
-def greet():
-    return "Mi casa es tu casa"
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
+mongo = PyMongo(app)
+
+
+@app.route("/")
+@app.route("/get_houses")
+def get_houses():
+    houses = mongo.db.houses.find()
+    return render_template("base.html", houses=houses)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "name": request.form.get("name").lower(),
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
+    return render_template("registration.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
