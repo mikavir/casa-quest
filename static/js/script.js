b37cb5bf6868d2bffeed25cb02bff3e50d6ec933
Mika Virtucio <mikavirtucio@gmail.com>
2024-05-05 12:05:09 +0000

    Refactor and update materialize web initialization

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,35 @@
 
 $(document).ready(function(){
-    $('.hamburger').on("click", function(){
-        $(".mobile-menu").toggleClass("open")
-    });
-   
-    const elems = document.querySelectorAll('select');
-    M.FormSelect.init(elems)
-    $(".datepicker").datepicker({
+  sideNav();
+  selectForm();
+  datepicker();
+  // filePath();
+  
+});
+
+function selectForm(){
+  const elems = document.querySelectorAll('select');
+  M.FormSelect.init(elems)
+}
+
+function datepicker(){
+    const elems = document.querySelectorAll('.datepicker');
+    const instances = M.Datepicker.init(elems, {
         format: "dd mmmm, yyyy",
         yearRange: 3,
         showClearBtn: true,
         i18n: {
           done: "Select"
         }
-      });
+    });
+}
 
+function sideNav() {
+  $('.hamburger').on("click", function(){
+    $(".mobile-menu").toggleClass("open")
+  });
+}
 
-});
-        
+function filePath(){
+  M.Forms.InitFileInputPath(document.querySelector('#image-url'));
+}
