1f6a58740260cd85f55f942bde263dec878be63a
Mika Virtucio <mikavirtucio@gmail.com>
2024-05-21 15:30:30 +0000

    Add email.js script

---
+++ static/js/email.js
@@ -0,0 +1,43 @@
+/* jshint esversion: 11, jquery: true */
+// Implemented from my(Mikaela)'s project https://github.com/mikavir/rx-decoder/blob/main/assets/scripts/email.js from email js.
+
+
+/** Function to send mail once validated using emailjs */
+function sendMail(contactForm){
+    if (validateForm(this)) {
+        emailjs.send("service_93gheht","template_pnxbum8",{
+            from_name: contactForm.name.value,
+            message: contactForm.message.value,
+            reply_to: contactForm.email.value
+            }).then(
+                (response) => {
+                    console.log("Success")
+                },
+                (error) => {
+                    console.log("FAILED", error);
+                }
+            );
+        return false;
+    } else {
+        this.addEventListener('submit', function (event) {
+            event.preventDefault();
+        });
+        console.log("ERROR: Unable to send form as the required fields have not been completed");
+    }
+ }
+
+/**Function to validate the form */
+function validateForm() {
+    let nameInput = document.forms.contactForm.name;
+    let emailInput = document.forms.contactForm.email;
+    let enquiryInput = document.forms.contactForm.message;
+    if (nameInput.value === "") {
+        return false;
+    } else if (emailInput.value === "") {        
+        return false;
+    } else if (enquiryInput.value === "") {
+        return false;
+    } else {
+        return true;
+    }
+}
