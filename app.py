655995ddbaab359a8d3f0572a0fc74806a23be82
Mika Virtucio <mikavirtucio@gmail.com>
2024-05-29 09:06:24 +0000

    Fix cloudinary config PEP8 compliancy

--- app.py
+++ app.py
@@ -28,9 +28,9 @@ mongo = PyMongo(app)
 
 # Configurations for Cloudinary API
 cloudinary.config(
-    cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    api_key = os.environ.get('CLOUDINARY_API_KEY'),
-    api_secret = os.environ.get('CLOUDINARY_API_SECRET')
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET')
 )
 
 
