fbcdf3319002a4ef70c563eb5b552e2a97078a36
Mika Virtucio <mikavirtucio@gmail.com>
2024-05-29 19:54:04 +0000

    Set minimum date to be that date for datepicker

--- static/js/script.js
+++ static/js/script.js
@@ -16,15 +16,23 @@ function selectForm(){
 }
 
 function datepicker(){
-    const elems = document.querySelectorAll('.datepicker');
-    const instances = M.Datepicker.init(elems, {
-        format: "dd mmmm, yyyy",
-        yearRange: 3,
-        showClearBtn: true,
-        i18n: {
-          done: "Select"
-        }
-    });
+  const elems = document.querySelectorAll('.datepicker');
+  const today = new Date();
+  const editDate = document.querySelector('#edit_date_viewing')
+  // Calculate the date 3 months from `today`
+  // Credits to Tim Nelson for helping calculate the three months
+  const threeMonthsFromToday = new Date(today);
+  threeMonthsFromToday.setMonth(today.getMonth() + 3);
+  const instances = M.Datepicker.init(elems, {
+      format: "dd mmmm, yyyy",
+      minDate: today,
+      maxDate: threeMonthsFromToday,
+      yearRange: 1,
+      showClearBtn: true,
+      i18n: {
+        done: "Select"
+      }
+  });
 }
 
 function sideNav() {
